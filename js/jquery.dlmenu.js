ce05313c520f1a1bf97ff553287762c0025b4793
Monica Cervantes <monica.cervantes@hunt.com.mx>
2016-01-20 14:12:57 -0600

    Cambios 2

--- js/jquery.dlmenu.js
+++ js/jquery.dlmenu.js
@@ -69,7 +69,7 @@
 			this.$trigger = this.$el.children( '.dl-trigger' );
 			this.$menu = this.$el.children( 'ul.dl-menu' );
 			this.$menuitems = this.$menu.find( 'li:not(.dl-back)' );
-			this.$el.find( 'ul.dl-submenu' ).prepend( '<li class="dl-back"><a href="#"><img src="menu/back.png"></a></li>' );
+			this.$el.find( 'ul.dl-submenu' ).prepend( '<li class="dl-back"><a href="#"><img style="margin-left:-34px" src="menu/back.png"></a></li>' );
 			this.$back = this.$menu.find( 'li.dl-back' );
 		},
 		_initEvents : function() {
